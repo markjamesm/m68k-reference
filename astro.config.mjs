56e6b627609c88f7e1b9d9ed2e90b8a811488c3d
Mark-James McDougall <contact@markjames.dev>
2026-02-16 18:42:28 -0500

    Add light and dark logos.

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,28 +1,38 @@
 // @ts-check
-import { defineConfig } from 'astro/config';
-import starlight from '@astrojs/starlight';
-import starlightThemeFlexoki from 'starlight-theme-flexoki'
+import { defineConfig } from "astro/config";
+import starlight from "@astrojs/starlight";
+import starlightThemeFlexoki from "starlight-theme-flexoki";
 
 // https://astro.build/config
 export default defineConfig({
-	integrations: [
-		starlight({
-			plugins: [starlightThemeFlexoki({accentColor: "blue",})],
-			title: 'M68k Reference',
-			social: [{ icon: 'github', label: 'GitHub', href: 'https://github.com/markjamesm/m68k-reference' }],
-			sidebar: [
-				{
-					label: 'Guides',
-					items: [
-						// Each item here is one entry in the navigation menu.
-						{ label: 'Example Guide', slug: 'guides/example' },
-					],
-				},
-				{
-					label: 'Reference',
-					autogenerate: { directory: 'reference' },
-				},
-			],
-		}),
-	],
+  integrations: [
+    starlight({
+      plugins: [starlightThemeFlexoki({ accentColor: "blue" })],
+      title: "M68k Reference",
+      logo: {
+        light: "./src/assets/light-logo.svg",
+        dark: "./src/assets/dark-logo.svg",
+      },
+      social: [
+        {
+          icon: "github",
+          label: "GitHub",
+          href: "https://github.com/markjamesm/m68k-reference",
+        },
+      ],
+      sidebar: [
+        {
+          label: "Guides",
+          items: [
+            // Each item here is one entry in the navigation menu.
+            { label: "Example Guide", slug: "guides/example" },
+          ],
+        },
+        {
+          label: "Reference",
+          autogenerate: { directory: "reference" },
+        },
+      ],
+    }),
+  ],
 });
